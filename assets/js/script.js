a31f3383b10c7d8d0e9de198e5a5377b443ca2e0
Marta <manta.101@hotmail.com>
2022-08-19 17:26:07 +0000

    Add code to addMove function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
     let button = document.getElementsByTagName("button")
     button[0].addEventListener("click", function() {
-        playGame();
+        startGame();
     });
-    playGame();
+    startGame();
 });
-function playGame() {
-    //Create an array of 20 random unique numbers
+
+  //Create game area with shuffled cards//
+ 
+function startGame() {
+    //Create an array of 20 random numbers//
     let arrayRandom = []
     while (arrayRandom.length < 20) {
         let number = Math.floor(Math.random()*20);
@@ -14,55 +17,67 @@ function playGame() {
             arrayRandom.push(number);
         }
     }
-    //With help of random array,
-    //Create an array of the images in random order.
-    let card = document.getElementsByClassName("card");
+    
+    //Create an array of the hidden images in random order//
+    let cards = document.getElementsByClassName("hidden");
     let shuffledCards = []
     for(let i = 0; i < 20; i++) {
-        shuffledCards.push(card[arrayRandom[i]])
+        shuffledCards.push(cards[arrayRandom[i]])
     }
     
+    //Create html of the cards and push to the html document//
     for (let i = 0; i < shuffledCards.length; i++) {
         let source = shuffledCards[i].getAttribute("src");
         let img = document.createElement("img");
         img.setAttribute("src", source);
         img.className = "card closed";
-        let cards = document.getElementsByClassName('cards')
-        cards[0].appendChild(img);
+        let cardDiv = document.getElementsByClassName('cards')
+        cardDiv[0].appendChild(img);
     };
+    cards = document.getElementsByClassName("closed");
     let turnedCards = [];
-    let closedCards = document.getElementsByTagName("img")
-    for (let i = 0; i < closedCards.length; i++) {
-        closedCards[i].addEventListener("click", function() {
-            openCard(i, turnedCards);
-        });
-    }
-};
-function openCard(i, turnedCards) {
-    let card = document.getElementsByTagName("img")[i];
-    card.classList.remove("closed");
-    turnedCards.push(card);
-    if (turnedCards.length === 2) {
-        checkPair(turnedCards);
+    for (let card of cards) {
+        card.addEventListener("click", function() {
+            openCard(turnedCards, card);
+        }, true);
     }
 };
 
-function checkPair(...args: []) {
-    let source1 = turnedCards[0].getAttribute("src");
-    let source2 = turnedCards[1].getAttribute("src");
-    if (source1 == source2) {
-        turnedCards[0].className = "card paired";
-        turnedCards[1].className = "card paired";
-    } else {
-        turnedCards[0].className = "card closed";
-        turnedCards[1].className = "card closed";
+ 
+ //Call checkPair function when 2 cards are clicked//
+ 
+function openCard(turnedCards, card) {
+    console.log(card.classList)
+    //console.log(card.classList)
+    card.classList.remove("closed");
+    console.log(card.classList)
+    //console.log(card.classList)
+    card.removeEventListener("click", function() {
+        openCard(turnedCards, card);
+    }, true);
+    turnedCards.push(card);
+    console.log(turnedCards)
+    //console.log(turnedCards)
+    if (turnedCards.length === 2) {
+        setTimeout( function() {
+            checkPair(turnedCards);
+            turnedCards.splice(0, 2);
+            addMove();
+        }, 500);    
     }
-    turnedCards.splice(0, 2);
 };
 
 function addMove() {
+
+    let moves = parseInt(document.getElementById("moves").innerText);
+    document.getElementById("moves").innerText = ++moves;
 };
+
 function addPair() {
+}
+function winGame() {
+
 };
+
 function recordCount() {
 };
