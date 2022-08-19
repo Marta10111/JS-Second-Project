3a261697b5acffffb7b0d1ccf3b30f785c921daf
Marta <manta.101@hotmail.com>
2022-08-19 16:26:25 +0000

    Create index.html file structure

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function() {
+    startGame();
+});
+
+function playGame() {
+
+};
+
+function openCard() {
+
+};
+
+function checkPair() {
+
+};
+
+function addMove() {
+
+};
+
+function addPair() {
+
+};
+
+recordCount() {
+
+};
